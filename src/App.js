865bf4606c12ec0390873273c8e1ba19461a65c7
Dan Kempton <kemptondan@gmail.com>
2021-03-08 08:54:20 -0500

    adding scale and temp pieces of state / adding temp change handlers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
 import TemperatureInput from "./components/TemperatureInput/temperatureinput";
 
 function App() {
+  const [scale, setScale] = useState("c");
+  const [temp, setTemp] = useState("");
+
+  const handleCelsiusChange = temp => {
+    setScale("c");
+    setTemp(temp);
+  };
+
+  const handleFahrenheitChange = temp => {
+    setScale("f");
+    setTemp(temp);
+  };
+
   return (
     <div>
-      <TemperatureInput scale="c" />
-      <TemperatureInput scale="f" />
+      <TemperatureInput
+        scale="c"
+        temp={temp}
+        handleTempChange={handleCelsiusChange}
+      />
+      <TemperatureInput
+        scale="f"
+        temp={temp}
+        handleTempChange={handleFahrenheitChange}
+      />
     </div>
   );
 }
--- src/components/TemperatureInput/temperatureinput.js
+++ src/components/TemperatureInput/temperatureinput.js
@@ -5,12 +5,16 @@ const tempScales = {
   f: "Fahrenheit",
 };
 
-function TemperatureInput({ scale }) {
+function TemperatureInput({ scale, temp, handleTempChange }) {
+  const handleInputChange = event => {
+    handleTempChange(event.target.value);
+  };
+
   return (
     <div>
       <fieldset>
         <h4>Enter Temperature in: {tempScales[scale]}</h4>
-        <input type="text" />
+        <input type="text" onChange={handleInputChange} />
       </fieldset>
     </div>
   );
